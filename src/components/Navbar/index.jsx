1fbb4a0ed85d9104f5d632cb2ce1d2c277791e65
João Victor Inácio <j.inacio.s@hotmail.com>
2025-08-08 21:10:20 -0300

    adicionando botão de trocar tema

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,6 +2,7 @@ import styles from "./Navbar.module.css";
 import logo from "../../assets/img/logo-joao.svg";
 import lightIcon from "../../assets/icons/light-icon.svg";
 import menuIcon from "../../assets/icons/menu-icon.svg";
+import { SwitchTheme } from "../SwitchTheme";
 
 const Navbar = () => {
 	return (
@@ -18,9 +19,10 @@ const Navbar = () => {
 				<p style={{ fontFamily: "Unica One", color: "#FFF" }}>J-INACIO</p>
 			</div>
 			<div className={styles.btnsWrapper}>
-				<img src={lightIcon} alt="" />
+				{/* <img src={lightIcon} alt="" /> */}
+				<SwitchTheme/>
 				<p>ENGLISH</p>
-				<img src={menuIcon} alt="icone menu" />
+				<img src={menuIcon} alt="icone menu" onClick={() => alert("ola")}/>
 			</div>
 		</div>
 	);
