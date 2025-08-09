08256346ff2c50ff8ebb7f5738fa858ce00507ec
João Victor Inácio <j.inacio.s@hotmail.com>
2025-08-09 13:54:41 -0300

    terminando estilização da sidebar

--- src/components/Menu/Sidebar/index.jsx
+++ src/components/Menu/Sidebar/index.jsx
@@ -1,15 +1,23 @@
 import styles from "./Sidebar.module.css";
+import { IoMdHome } from "react-icons/io";
+import { IoPersonSharp, IoLogoLinkedin, IoDocumentText, IoLogoGithub, IoCloseSharp  } from "react-icons/io5";
+import { AiFillProject } from "react-icons/ai";
+import { SiHyperskill } from "react-icons/si";
 
 const Sidebar = ({ isOpen, onClose }) => {
 	return (
-		<div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+		<div className={`${styles.sidebar} ${isOpen ? styles.open : ""} glassFilter`}>
 			<button className={styles.closeButton} onClick={onClose}>
-				&times;
+				<IoCloseSharp color="var(--text-color)"/>
 			</button>
 			<ul>
-                <li><a href="#">Início</a></li>
-                <li><a href="#">Projetos</a></li>
-                <li><a href="#">Contato</a></li>
+                <li cla><a href="#"><i><IoMdHome/></i>Início</a></li>
+                <li><a href="#"><i><IoPersonSharp /></i>Sobre mim</a></li>
+                <li><a href="#"><i><AiFillProject /></i>Projetos</a></li>
+                <li><a href="#"><i><SiHyperskill /></i>Habilidades</a></li>
+                <li><a href="#"><i><IoDocumentText /></i>Currículo</a></li>
+                <li><a href="#"><i><IoLogoLinkedin /></i>Linkedin</a></li>
+                <li><a href="#"><i><IoLogoGithub /></i>Github</a></li>
             </ul>
 		</div>
 	);
