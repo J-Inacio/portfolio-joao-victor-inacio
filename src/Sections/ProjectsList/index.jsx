f830a1b368288fdc382dcda35cc261cf8eb39ccb
João Victor Inácio <j.inacio.s@hotmail.com>
2025-08-12 18:33:40 -0300

    finalizando seção projetos

--- src/Sections/ProjectsList/index.jsx
+++ src/Sections/ProjectsList/index.jsx
@@ -1,26 +1,71 @@
 import Project from "../../components/Project";
 import styles from "./ProjectsList.module.css";
 
-const ProjectsList = ({onImageClick}) => {
+const projectsData = [
+	{
+		img: "https://github.com/J-Inacio/psicoser/blob/main/src/assets/img/screenshot3.png?raw=true",
+		skills: ["Javascript", "HTML", "CSS", "Webpack", "Figma"],
+		projectText: `O Psicoser é uma landing page desenvolvida para um consultório psicológico real, com o objetivo de atrair mais pacientes e ampliar a divulgação dos serviços.
+		O projeto foi criado com foco em acessibilidade, clareza e impacto visual, garantindo que a mensagem central do consultório fosse transmitida de forma acolhedora e profissional.`,
+		title: "Psicoser",
+		repositoryLink: "https://github.com/J-Inacio/psicoser",
+		deployLink: "https://psicoser.vercel.app/",
+	},
+	{
+		img: "https://raw.githubusercontent.com/PedroDeVvV/GasFinder/initial/.github/a1.png",
+		skills: [
+			"Javascript",
+			"HTML",
+			"CSS",
+			"React JS",
+			"Figma",
+			"Node JS",
+			"Google Maps API",
+		],
+		projectText: `GasFinder, é um projeto que facilita a busca pelos melhores preços de combustíveis e postos próximos a você! Desenvolvido como Trabalho de Conclusão de Curso (TCC) na ETEC de Embu, o GasFinder utiliza dados atualizados da ANP (Agência Nacional do Petróleo) para fornecer informações precisas e confiáveis.`,
+		title: "GasFinder",
+		repositoryLink: "https://github.com/J-Inacio/GasFinder",
+		deployLink: "https://github.com/J-Inacio/GasFinder",
+	},
+	{
+		img: "https://github.com/J-Inacio/transactions-with-api/raw/main/transactions-with-api.gif",
+		skills: ["Javascript", "HTML", "CSS", "json-server"],
+		projectText: `Este projeto foi desenvolvido para exercitar o uso de requisições a uma API, utilizando a ferramenta json-server para simular uma API RESTful a partir de um arquivo JSON. No projeto é possível adicionar, editar e remover transações utilizando métodos: POST | PUT | DELETE`,
+		title: "Gerenciador de Transações",
+		repositoryLink: "https://github.com/J-Inacio/transactions-with-api",
+		deployLink: "https://github.com/J-Inacio/transactions-with-api",
+	},
+];
+
+const ProjectsList = ({ onImageClick }) => {
 	return (
 		<section className={`${styles.projectsContainer} glassFilter`}>
 			<h2>Projetos</h2>
 			<ul>
-				<Project
-					img={
-						"https://github.com/J-Inacio/psicoser/blob/main/src/assets/img/screenshot3.png?raw=true"
+				{projectsData.map(
+					({ img, skills, deployLink, projectText, repositoryLink, title }) => {
+						return (
+							<Project
+								img={img}
+								skills={skills}
+								projectText={projectText}
+								title={title}
+								deployLink={deployLink}
+								repositoryLink={repositoryLink}
+								onImageClick={onImageClick}
+							/>
+						);
 					}
-					skills={["Javascript", "HTML"]}
-					projectText={
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed tristique mauris, et consectetur nulla. Vivamus eu sapien quis odio fermentum egestas varius ut mauris. Sed ut velit eros. Aliquam erat volutpat. Donec a ante orci. Fusce tristique ac leo at tempus. Etiam pharetra eget orci in eleifend. Phasellus sit amet nibh quam. Cras sodales lorem vitae elit sollicitudin dignissim at et felis. Mauris eu felis sed lorem volutpat rhoncus. Vestibulum aliquet, velit placerat elementum feugiat, nunc ipsum viverra lectus, sed dapibus neque enim sed augue."
-					}
-					title={"Psicoser"}
-					deployLink={"https://github.com/J-Inacio/psicoser"}
-					repositoryLink={"https://psicoser.vercel.app/"}
-					onImageClick={onImageClick}
-				/>
+				)}
 			</ul>
-            <a href="https://github.com/J-Inacio?tab=repositories" target="_blank" rel="noopener noreferrer" className= {styles.moreProjects}><h3>Ver mais projetos</h3></a>
+			<a
+				href="https://github.com/J-Inacio?tab=repositories"
+				target="_blank"
+				rel="noopener noreferrer"
+				className={styles.moreProjects}
+			>
+				<h3>Ver mais projetos</h3>
+			</a>
 		</section>
 	);
 };
