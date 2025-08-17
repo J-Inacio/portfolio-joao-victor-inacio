b6a6488f27c7ff0e2843250938cb8380f5be30e4
João Victor Inácio <j.inacio.s@hotmail.com>
2025-08-17 20:12:04 -0300

    seção soft skills finalizada

--- src/Sections/Skills/index.jsx
+++ src/Sections/Skills/index.jsx
@@ -3,6 +3,9 @@ import Technologies from "../../components/Technologies";
 import { Element } from "react-scroll";
 import { useTranslation } from "react-i18next";
 import useScrollAnimation from "../../hooks/useScrollAnimation";
+import SoftSkills from "../../components/SoftSkills";
+import { FaHandshakeSimple, FaMagnifyingGlass } from "react-icons/fa6";
+import { IoExtensionPuzzleSharp, IoHeartCircle, IoRocket, IoSparkles, IoSearchSharp, IoShuffle, IoLibrary, IoPeople} from "react-icons/io5";
 
 const technologiesData = [
 	{
@@ -59,28 +62,97 @@ const technologiesData = [
 	},
 ];
 
+const softSkills = [
+	{
+		icon: <IoPeople />,
+		name: "softSkill.teamwork.title",
+		text1: "softSkill.teamwork.txtP1",
+		text2: "softSkill.teamwork.txtP2",
+	},
+	{
+		icon: <IoRocket /> ,
+		name: "softSkill.proactive.title",
+		text1: "softSkill.proactive.txtP1",
+		text2: "softSkill.proactive.txtP2"
+	},
+	{
+		icon: <IoExtensionPuzzleSharp />,
+		name: "softSkill.resolution.title",
+		text1: "softSkill.resolution.txtP1",
+		text2: "softSkill.resolution.txtP2"
+	},
+	{
+		icon: <IoSearchSharp /> ,
+		name: "softSkill.curious.title",
+		text1: "softSkill.curious.txtP1",
+		text2: "softSkill.curious.txtP2"
+	},
+	{
+		icon: <IoShuffle />,
+		name: "softSkill.flexible.title",
+		text1: "softSkill.flexible.txtP1",
+		text2: "softSkill.flexible.txtP2"
+	},
+	{
+		icon: <IoHeartCircle />,
+		name: "softSkill.empathetic.title",
+		text1: "softSkill.empathetic.txtP1",
+		text2: "softSkill.empathetic.txtP2"
+	},
+	{
+		icon: <IoSparkles />,
+		name: "softSkill.creative.title",
+		text1: "softSkill.creative.txtP1",
+		text2: "softSkill.creative.txtP2"
+	},
+	{
+		icon: <IoLibrary />,
+		name: "softSkill.focused.title",
+		text1: "softSkill.focused.txtP1",
+		text2: "softSkill.focused.txtP2"
+	},
+];
+
 const Skills = () => {
 	const { t } = useTranslation();
 	const { ref, inView } = useScrollAnimation();
 	return (
-		<Element className={`${styles.mainContainerSkills} glassFilter`}>
-			<h2>{t("skills.skillsTitle")}</h2>
-			<section
-				id="#skills"
-				ref={ref}
-				className={`
+		<Element id="#skills">
+			<section className={`${styles.mainContainerSkills} glassFilter`}>
+				<h2>{t("skills.skillsTitle")}</h2>
+				<div
+					ref={ref}
+					className={`
 				${styles.skillsContainer}
 				fade-in-section
 				${inView ? "is-visible" : ""}
 				`}
-			>
-				{technologiesData.map((tec) => (
-					<Technologies
-						key={tec.techName}
-						src={tec.src}
-						techName={tec.techName}
-					/>
-				))}
+				>
+					{technologiesData.map((tec) => (
+						<Technologies
+							key={tec.techName}
+							src={tec.src}
+							techName={tec.techName}
+						/>
+					))}
+				</div>
+			</section>
+
+			<section className={`${styles.mainContainerSkills} glassFilter`}>
+				<h2>Soft Skills</h2>
+				<ul className={styles.SoftSkillsMainContainer}>
+					{softSkills.map((softSkill) => {
+						return (
+							<SoftSkills
+								key={softSkill.name}
+								icon={softSkill.icon}
+								skillName={t(softSkill.name)}
+								skillTxt1={t(softSkill.text1)}
+								skillTxt2={t(softSkill.text2)}
+							/>
+						);
+					})}
+				</ul>
 			</section>
 		</Element>
 	);
---
+++ src/components/SoftSkills/index.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import styles from "./SoftSkills.module.css";
+import { AnimatePresence, motion } from "motion/react";
+import { MdOutlineExpandMore } from "react-icons/md";
+
+const SoftSkills = ({ icon, skillName, skillTxt1, skillTxt2 }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<li className={` ${styles.mainSoftSkills}`}>
+			<div
+				className={` ${styles.softSkills}`}
+				onClick={() => setIsOpen(!isOpen)}
+			>
+				<div className={styles.wrapper}>
+					{icon}
+					<span>{skillName}</span>
+				</div>
+				<MdOutlineExpandMore />
+			</div>
+			<AnimatePresence>
+				{isOpen && (
+					<motion.aside
+						initial={{ opacity: 0, y: -50 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: -10 }}
+						className={`liteGlassFilter ${styles.softSkillsAside}`}
+					>
+						<p>{skillTxt1}</p>
+						<p>{skillTxt2}</p>
+					</motion.aside>
+				)}
+			</AnimatePresence>
+		</li>
+	);
+};
+
+export default SoftSkills;
